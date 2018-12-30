4d94a5e217b0b26600ce4f44d0b6b8a87a7dab1d
pangduckwai <paul.lai@sea9.org>
2018-12-30 13:16:52 +0800

    Work in progress (2)

--- utest2.js
+++ utest2.js
@@ -1,44 +1,57 @@
 const cnst = require('./constants');
 
 const COMMAND = {
-    attack: {
-        exec: (attacker, defender) => {
-            if (attacker.army <= 1) return null;
-
-            let redDice, whiteDice;
-            switch (attacker.army) {
-                case 2:  redDice = [throwDice()]; break;
-                case 3:  redDice = [throwDice(), throwDice()]; break;
-                default: redDice = [throwDice(), throwDice(), throwDice()]; break;
-            }
-            whiteDice = (defender.army > 1) ? [throwDice(), throwDice()] : [throwDice()];
-
-            redDice.sort().reverse();
-            whiteDice.sort().reverse();
-
-            console.log("Red", redDice);
-            console.log("White", whiteDice);
-        },
-        event: ["attackCommenced", "beingAttacked"]
-    }
+	attack: {
+		event: ["attackCommenced", "beingAttacked"]
+	}
 };
 
 const EVENT = {
-       attackCommenced: {
+	attackCommenced: {
+		exec: (...args) => {
+			battle(args[0], args[1]);
+		}
+	}, beingAttacked: {
+		exec: (...args) => { }
+	}
+};
 
-    }, beingAttacked: {
-
-    }
+let execute = (command, ...args) => {
+	command.event.forEach((eid) => {
+		EVENT[eid].exec(...args);
+	});
 };
 
 let throwDice = () => {
-    return Math.floor(Math.random() * Math.floor(6)) + 1;
+	return Math.floor(Math.random() * Math.floor(6)) + 1;
+};
+
+let battle = (attacker, defender) => {
+	if ((typeof attacker === 'undefined') || (typeof defender === 'undefined'))
+		return {"error": "Attacker and defender must be defined"};
+	if (attacker.army <= 1)
+		return {"message": "Insufficient army"};
+
+	let redDice, whiteDice;
+	switch (attacker.army) {
+		case 2:  redDice = [throwDice()]; break;
+		case 3:  redDice = [throwDice(), throwDice()]; break;
+		default: redDice = [throwDice(), throwDice(), throwDice()]; break;
+	}
+	whiteDice = (defender.army > 1) ? [throwDice(), throwDice()] : [throwDice()];
+
+	redDice.sort().reverse();
+	whiteDice.sort().reverse();
+
+	for (let i = 0; i < Math.min(redDice.length, whiteDice.length); i ++) {
+		
+	}
 };
 
 let runTests = () => {
-    let p1 = {army: 2};
-    let p2 = {army: 2};
-    COMMAND['attack'].exec(p1, p2);
+	let t1 = {army: 10};
+	let t2 = {army: 7};
+	execute(COMMAND['attack'], t1, t2);
 };
 
 module.exports = () => {
